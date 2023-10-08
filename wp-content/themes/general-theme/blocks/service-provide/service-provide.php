5a19219e8caef5414a342aaf95d31726d9cb8dd5
Dragan Mojsilovic <gasa338@gmail.com>
2023-10-08 18:30:18 +0200

    Dodavanej servise provide spaces
    Dodavanej servise provide spaces

--- wp-content/themes/general-theme/blocks/service-provide/service-provide.php
+++ wp-content/themes/general-theme/blocks/service-provide/service-provide.php
@@ -1,11 +1,27 @@
 <?php
 $small_title = get_field( 'service_provide_small_title' );
 $title       = get_field( 'service_provide_title' );
+
+$spaces = '';
+switch (get_field( 'service_provide_spaces' )) {
+	case 'space':
+		$spaces = 'space';
+		break;
+	case 'space-top':
+		$spaces = 'space-top';
+		break;
+	case 'space-bottom':
+		$spaces = 'space-bottom';
+		break;
+	default:
+		break;
+}
 ?>
 <!--==============================
 Service Area 02
 ==============================-->
-<div class="service-area-2 space overflow-hidden">
+<div class="service-area-2 <?php echo $spaces; ?> overflow-hidden">
+    <?php if ($small_title != '' || $title !="" ): ?>
     <div class="container">
         <div class="title-area text-center">
 			<?php if ( $small_title != '' ): ?>
@@ -14,6 +30,7 @@ Service Area 02
             <h2 class="sec-title"><?php echo $title; ?></h2>
         </div>
     </div>
+    <?php endif; ?>
     <div class="container">
         <div class="row global-carousel service-slider-2 slider-shadow" data-slide-show="3" data-ml-slide-show="3"
              data-lg-slide-show="3" data-md-slide-show="2" data-sm-slide-show="1" data-xs-slide-show="1"
