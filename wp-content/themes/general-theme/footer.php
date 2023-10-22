3936e2e84b5c6bf81c102cfe4d987697fba8daae
Dragan Mojsilovic <gasa338@gmail.com>
2023-10-22 10:47:11 +0200

    testiranje
    testr

--- wp-content/themes/general-theme/footer.php
+++ wp-content/themes/general-theme/footer.php
@@ -203,16 +203,16 @@
         obj.removeAttribute('data-bg-src')
     })
     */
-    (function ($) {
-        "use strict";
-        if ($("[data-bg-src]").length > 0) {
-            $("[data-bg-src]").each(function () {
-                var src = $(this).attr("data-bg-src");
-                $(this).css("background-image", "url(" + src + ")");
-                $(this).removeAttr("data-bg-src").addClass("background-image");
-            });
-        }
-    })(jQuery);
+    // (function ($) {
+    //     "use strict";
+    //     if ($("[data-bg-src]").length > 0) {
+    //         $("[data-bg-src]").each(function () {
+    //             var src = $(this).attr("data-bg-src");
+    //             $(this).css("background-image", "url(" + src + ")");
+    //             $(this).removeAttr("data-bg-src").addClass("background-image");
+    //         });
+    //     }
+    // })(jQuery);
 
 </script>
 </body>
